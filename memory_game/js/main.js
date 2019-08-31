4f6b7c1864942dd910b6bdfab836007a246cd2c9
FIRST_Ashley LAST_Gargalikis <ashgargalikis@gmail.com>
2019-08-31 12:00:13 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,1 +1,21 @@
-console.log("Up and running!");
+
+var cards = ["Queen", "Queen", "King", "King"];
+var cardsInPlay = [];
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again");	
+	}
+}
+function flipCard(cardId) {
+console.log("User flipped" + cards [cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2){
+		checkForMatch(); }
+}
+
+
+
+flipCard(0);
+flipCard(2);
