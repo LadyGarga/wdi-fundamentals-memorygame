df2eb93c66b09cf444eac72dc6b36a70694a9d5a
FIRST_Ashley LAST_Gargalikis <ashgargalikis@gmail.com>
2019-09-07 12:15:50 -0700

    final game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -27,15 +27,29 @@ const checkForMatch = () => {
 	 if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
-		alert("Sorry, try again");}	
+		alert("Sorry, try again");
 	}
+}	
 
-const flipCard = (cardId) => {
+const flipCard = function() {
+	let cardId = this.getAttribute('data-id');
 	console.log(" User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute('src',cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
-	checkForMatch();
+	checkForMatch(); 
 	}
 }
+
+const createBoard = () => {
+	for (let i = 0; i < cards.length; i ++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+		}
+	} 
+	createBoard();
